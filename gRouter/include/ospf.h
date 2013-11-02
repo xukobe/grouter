49d361c9a066008db6ab5a5080f7d4c0d35634a2
Xuepeng <xukobe@gmail.com>
2013-11-02 14:16:41 -0400

    Add neighbor array and hello data struct

--- gRouter/include/ospf.h
+++ gRouter/include/ospf.h
@@ -8,7 +8,10 @@
 #define __OSPF_H
 
 #include "ip.h"
+#include "gnet.h"
 
+#define DEFAULT_HELLO_INTERVAL 10
+#define DEFAULT_DEAD_INTERVAL 40
 
 typedef struct _ospf_header_t
 {
@@ -33,6 +36,37 @@ typedef struct _ospf_lsa_header_t
     uint16_t checksum;
     uint16_t len;
 }ospf_lsa_header_t;
+
+typedef struct _neigh_ip{
+    uchar ip[4];
+}neigh_ip;
+
+typedef struct _ospf_hello_data_t{
+    uchar netmask[4];
+    uint16_t helloInterval;
+    uint8_t options;
+    uint8_t priority;
+    uint32_t deadInterval;
+    uchar designatedIP[4];
+    uchar backupdesignateIP[4];
+    neigh_ip neighbors[MAX_INTERFACES];
+}ospf_hello_data;
+
+typedef struct _ospf_neigh_entry_t
+{
+    uchar neighbor[4];
+    bool isEmpty;
+    int interface_id;
+    uchar neighbormask[4];
+}ospf_neigh_entry;
+
+typedef struct _ospf_neigh_array_t
+{
+    int count;
+    ospf_neigh_entry elem[MAX_INTERFACES];
+};
+
+int OSPFInit();
 
 void *OSPFSendHelloMessage(void* ptr);
 int OSPFInitHelloThread();
